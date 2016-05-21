232d522e4ba2557b71d696490b804b420b748ac8
Zarnaik <sennemalcorps@gmail.com>
2016-05-21 10:58:49 +0200

    Fixed writing the subtitle, it now saves to the correct directory Removed personal directory path

--- OpenSubDownloader.py
+++ OpenSubDownloader.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from os import remove
 from base64 import b64decode
 from subprocess import check_output
@@ -8,11 +9,11 @@ import gzip
 # TODO support arguments (login, langage, ...)
 
 # read in the file(s) for which subtitles are needed
-series = check_output('cd ~/Downloads/Torrents/SERIES && zenity --file-selection --multiple',
+series = check_output('zenity --file-selection --multiple',
                       shell=True).rstrip().split("|")
 
 server = Server('http://api.opensubtitles.org/xml-rpc')
-loginInfo = server.LogIn('', '', 'en', 'OSTestUserAgent')
+loginInfo = server.LogIn('', '', 'en', 'OSTestUserAgent')     # TODO request personal uagent
 token = loginInfo['token']
 # TODO error handling
 language = 'eng'  # TODO language input from user
@@ -20,15 +21,15 @@ language = 'eng'  # TODO language input from user
 
 for serie in series:
     name = serie.split('/')[-1]
-    print 'name', name
+    saveName = serie[:-3]
     searchName = name
-    # TODO request personal uagent
+    print 'name', name
 
     while True:
         subResults = server.SearchSubtitles(token, [{'sublanguageid': language, 'query': searchName}])
         if len(subResults['data']) == 0 :
             try:
-                check_output('zenity --question --text=\'No subtitles found for video file: <b>%s</b>\\n\\nContinue?\'' % name, shell=True)
+                check_output('zenity --question --text=\'No subtitles found for the query: <b>%s</b>\\n\\nContinue?\'' % searchName, shell=True)
                 break
             except:
                 searchName = check_output('zenity --entry --text=\'Please enter the name you would like to search with.\' --entry-text=%s' % name, shell=True)
@@ -49,11 +50,11 @@ for serie in series:
         continue
 
     try:
-        chosensub = int(check_output("cd ~/Downloads/Torrents/SERIES && zenity --text='Video file: <b>%s</b>' "
+        chosensub = int(check_output("zenity --text='Video file: <b>%s</b>' "
                                      "--height=570 --width=500 --list --column=ID --column=Subtitle%s" % (name, subs),
                                      shell=True).rstrip().split("|")[0])
     except:
-        print 'Canceled or other error due to not selecting a subtitle but continuing'
+        print 'Canceled or other error due to not selecting a subtitle before continuing'
         continue
 
     print chosensub
@@ -64,13 +65,13 @@ for serie in series:
         continue
         # TODO error handling sufficient?
     zipdata = b64decode(b64zipdata['data'][0]['data'])
-    with open('%s.gz' % name[:-4], 'w') as zipfile:
+    with open('%sgz' % saveName, 'w') as zipfile:
         zipfile.write(zipdata)
 
-    with gzip.open('%s.gz' % name[:-4], 'rb') as infile:
-        with open('%s.srt' % name[:-4], 'w') as outfile:
+    with gzip.open('%sgz' % saveName, 'rb') as infile:
+        with open('%ssrt' % saveName, 'w') as outfile:
             for line in infile:
                 outfile.write(line)
 
-    remove('%s.gz' % name[:-4])
+    remove('%sgz' % saveName)
 
